085bbfa861768b3739b5389c0f11ae3be705ca27
Peter Somerville <pesomerville@gmail.com>
2018-05-04 12:25:50 +0800

    still setting things up

--- apps/chat/consumers.py
+++ apps/chat/consumers.py
@@ -1,4 +1,5 @@
 import json
+from asgiref.sync import async_to_sync
 from django.conf import settings
 from channels.generic.websocket import JsonWebsocketConsumer
 
@@ -8,6 +9,22 @@ class ChatConsumer(JsonWebsocketConsumer):
         if 'user_id' in self.scope['session']:
             print('CONNECTION FROM SOCKET INITIATED')
             self.accept()
+            self.room_name = self.scope['url_route']['kwargs']['room_name']
+
+            attach_to = async_to_sync(self.channel_layer.group_add)
+            attach_to(self.room_name, self.channel_name)
+
+            broadcast_to = async_to_sync(self.channel_layer.group_send)
+            broadcast_to(self.room_name, {
+                'type': 'send.notification',
+                'success': True,
+                'notification': '{} has joined the room'.format(self.scope['session']['username'])
+            })
+
+            self.send_json({
+                'success': True
+            })
+
         else:
             print('CONNECTION CLOSED USER IS NOT LOGGED IN')
             self.close()
@@ -17,3 +34,10 @@ class ChatConsumer(JsonWebsocketConsumer):
 
     def disconnect(self, close_code):
         pass
+
+
+    def send_notification(self, event):
+        self.send_json({
+            'success': event['success'],
+            'notification': event['notification']
+        })
